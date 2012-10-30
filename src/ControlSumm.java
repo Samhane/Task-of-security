785255d1c22802ce975bdd70e5a9a21842f26071
Denis Karyakin <samhane.me@gmail.com>
2012-10-30 22:53:52 +0400

    Working find file by crc in path. Using CRC32, from java.util.zip.CRC32

--- src/ControlSumm.java
+++ src/ControlSumm.java
@@ -1,5 +1,10 @@
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.zip.CRC32;
+import java.util.zip.Checksum;
 
 public class ControlSumm {
     public void setOriginalControlSumm(String file) {
@@ -11,7 +16,8 @@ public class ControlSumm {
     public ArrayList<String> findFile(String path) {
         String[] dirForSearch = new File(path).list();
         ArrayList<String> result = new ArrayList<String>();
-        for (String currentPath : dirForSearch) {
+        for (String current : dirForSearch) {
+            String currentPath = path.concat(current);
             File currentFile = new File(currentPath);
             if (currentFile.isDirectory()) {
                 result.addAll(findFile(currentPath));
@@ -26,7 +32,24 @@ public class ControlSumm {
     }
 
     private String checkControlSumm(String file) {
-        String result = "";
-        return result;
+        byte[] buf = new byte[8000];
+        int nLength = 0;
+        Checksum cs = new CRC32();
+
+        try {
+            FileInputStream inputFile = new FileInputStream(file);
+
+            while ((nLength = inputFile.read(buf)) >= 0) {
+                cs.update(buf, 0, nLength);
+            }
+
+            inputFile.close();
+        } catch (FileNotFoundException notFound) {
+            //скорее всего, в имени файла пробел, а с такими мы не работаем
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return Long.toString(cs.getValue());
     }
 }
