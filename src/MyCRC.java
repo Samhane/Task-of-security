b01ee30ac36459f035a5cb91e6bb06b03dab0933
Denis Karyakin <samhane.me@gmail.com>
2012-10-31 14:08:19 +0400

    In MyCRC add cdraft compute CRC code

--- src/MyCRC.java
+++ src/MyCRC.java
@@ -8,6 +8,10 @@ public class MyCRC implements Checksum {
     private int crc_table[];
 
     public MyCRC() {
+        this.crc_table = new int[256];
+        makeCRCTable();
+        printCRCTable();
+        reset();
         crc = new CRC32();
     }
 
@@ -18,16 +22,44 @@ public class MyCRC implements Checksum {
 
     @Override
     public void update(byte[] b, int off, int len) {
+        int c = ~this.value;
+        while (--len >= 0) {
+            c = crc_table[(c ^ b[off++]) & 0xff] ^ (c >>> 8);
+        }
+        this.value = ~c;
+
         crc.update(b, off, len);
     }
 
     @Override
     public long getValue() {
+        this.result = (long) this.value & 0xffffffffL;
         return crc.getValue();
     }
 
     @Override
     public void reset() {
         this.value = 0;
+    }
+
+    private void makeCRCTable() {
+        for (int i = 0; i < 256; i++) {
+            int c = i;
+            for (int j = 8; --j >= 0; ) {
+                if ((c & 1) != 0)
+                    c = 0xEDB88320 ^ (c >>> 1);
+                else
+                    c = c >>> 1;
+            }
+            crc_table[i] = c;
+        }
+    }
+
+    public void printCRCTable() {
+        int count = 0;
+        for (int printcrc : crc_table) {
+            System.out.print(Integer.toHexString(printcrc) + " ");
+        }
+        System.out.println();
     }
 }
