28406dbeb91ab83f4ab88b06b571d460ebe0d9be
Denis Karyakin <samhane.me@gmail.com>
2012-11-04 23:35:02 +0400

    Working find file by control sum

--- src/MyCRC.java
+++ src/MyCRC.java
@@ -1,40 +1,32 @@
-import java.util.zip.CRC32;
 import java.util.zip.Checksum;
 
 public class MyCRC implements Checksum {
     private int value;
-    private CRC32 crc;
-    private long result;
     private int crc_table[];
 
     public MyCRC() {
-        //this.crc_table = new int[256];
-        //makeCRCTable();
+        this.crc_table = new int[256];
+        makeCRCTable();
         //printCRCTable();
-        //reset();
-        crc = new CRC32();
+        reset();
     }
 
     @Override
     public void update(int b) {
-        crc.update(b);
     }
 
     @Override
     public void update(byte[] b, int off, int len) {
-        /*int c = ~this.value;
+        int c = ~this.value;
         while (--len >= 0) {
             c = crc_table[(c ^ b[off++]) & 0xff] ^ (c >>> 8);
         }
-        this.value = ~c;  */
-
-        crc.update(b, off, len);
+        this.value = ~c;
     }
 
     @Override
     public long getValue() {
-        /*this.result = (long) this.value & 0xffffffffL;  */
-        return crc.getValue();
+        return (long) this.value & 0xffffffffL;
     }
 
     @Override
