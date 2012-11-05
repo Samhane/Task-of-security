e902deacd73a0cd9bff8c97c4db75dd1299952b5
Denis Karyakin <samhane.me@gmail.com>
2012-11-05 15:07:14 +0400

    Add working (draftly) find file by signature

--- src/Signature.java
+++ src/Signature.java
@@ -1,3 +1,9 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 public class Signature extends ParentSecure {
     private String originalSignature;
 
@@ -6,7 +12,34 @@ public class Signature extends ParentSecure {
     }
 
     private String checkSignature(String file) {
-        return null;
+        byte[] buf = new byte[8000];
+        byte[] result = new byte[0];
+        int nLength = 0;
+
+        try {
+            FileInputStream inputFile = new FileInputStream(file);
+            nLength = inputFile.read(buf);
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            byte[] current = new byte[2000];
+            for (int i = 1000, j = 0; i < 3000 && i < buf.length && j < current.length; i++, j++) {
+                current[j] = buf[i];
+            }
+            result = md.digest(current);
+            inputFile.close();
+        } catch (FileNotFoundException notFound) {
+            System.out.println("Что за файл такой - " + file);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+
+        long answer = 1;
+        for (byte aResult : result) {
+            answer *= aResult;
+        }
+
+        return Long.toHexString(answer);
     }
 
     @Override
